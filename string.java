fda3141a09983b0df7628876771fa0555df0d463
21cse088 <111353143+21cse088@users.noreply.github.com>
2022-10-07 21:05:05 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+
+public class stringhandling {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+     
+      func n = new func();
+        
+         String[] str ={"java virtual machine","local area network","integrated Development Environment","java Development Kit"};
+         
+       
+    for(int i=1;i<str.length;i++)
+    {
+      
+          System.out.println(str[i]);
+          String s = str[i];
+          String[] arr = s.split(" ");
+          for (String a : arr)
+               System.out.println(a);
+          n.acro(arr);
+      }
+     
+    }
+    
+   
+}
+
+class func{
+    public void acro(String a[]){
+        String m="";
+        for(int j =0;j<a.length;j++){
+            m=m + String.valueOf(a[j].charAt(0)).toUpperCase();
+           }
+        System.out.println(m);
+    }
+}
